7005351c0c030eea3fd1ffaa2802f342167317cf
Patrick Gunderson <patrick@toolofna.com>
2015-01-14 15:02:43 -0800

    Added Cycle button, started adding soundcloud to api twitter

--- src/front-end/js/views/pages/home-view.js
+++ src/front-end/js/views/pages/home-view.js
@@ -4,15 +4,25 @@ var AbstractPage = require("./Page");
 
 //abstract page class
 var Page = AbstractPage.extend({
-	row:0,
-	col:0,
-	el: "#home",
-	events: {
-		"click button": "onClickTriggerButton"
-	},
-	onClickTriggerButton: function(e){
-		$.get("http://localhost:3030/pulse/trigger/" + $(e.target).data("trigger"));
-	}
+    row: 0,
+    col: 0,
+    el: "#home",
+    events: {
+        "click button.trigger": "onClickTriggerButton",
+        "click button.triggerAll": "onClickTriggerAllButton",
+        "click button.getSongsFromTwitter": "onClickGetSongsFromTwitter"
+    },
+    onClickTriggerButton: function(e) {
+        $.get("http://localhost:3030/pulse/trigger/" + $(e.target).data(
+            "trigger"));
+    },
+    onClickTriggerAllButton: function(e) {
+        $.get("http://localhost:3030/pulse/trigger/3");
+        $.get("http://localhost:3030/pulse/trigger/4");
+    },
+    onClickGetSongsFromTwitter: function() {
+        $.get("http://localhost:3030/songs/cycle");
+    }
 });
 
 module.exports = Page;
